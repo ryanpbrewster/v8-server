8ec01cf66de05d2c729a1499d8193f3f6b123906
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-08-08 15:39:48 -0700

    mutex and mutation apis

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,14 @@ use lazy_static::lazy_static;
 use log::info;
 use std::collections::BTreeMap;
 use std::convert::Infallible;
-use std::sync::atomic::{AtomicI32, Ordering};
+use std::sync::Mutex;
 use structopt::StructOpt;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
-
     let opts = Opts::from_args();
-    let seed = opts.seed;
-    info!("starting with seed {}", seed);
 
     let platform = v8::new_default_platform().unwrap();
     v8::V8::initialize_platform(platform);
@@ -30,12 +27,8 @@ async fn main() {
     warp::serve(routes).run(([127, 0, 0, 1], 9000)).await;
 }
 
-static COUNTER: AtomicI32 = AtomicI32::new(0);
 lazy_static! {
-    static ref KV: BTreeMap<String, String> = vec![("a", "Hello"), ("b", "Goodbye")]
-        .into_iter()
-        .map(|(k, v)| (k.to_owned(), v.to_owned()))
-        .collect();
+    static ref KV: Mutex<BTreeMap<String, String>> = Mutex::new(BTreeMap::new());
 }
 async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
     let isolate = &mut v8::Isolate::new(Default::default());
@@ -56,9 +49,9 @@ async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
                 .to_string(scope)
                 .unwrap()
                 .to_rust_string_lossy(scope);
-            let default = String::new();
-            let value = KV.get(&key).unwrap_or(&default);
-            rv.set(v8::String::new(scope, value).unwrap().into());
+            if let Some(value) = KV.lock().unwrap().get(&key) {
+                rv.set(v8::String::new(scope, value).unwrap().into());
+            }
         },
     );
     let get_fn_name = v8::String::new(scope, "get").unwrap();
@@ -69,15 +62,19 @@ async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
         |scope: &mut v8::HandleScope,
          args: v8::FunctionCallbackArguments,
          mut rv: v8::ReturnValue| {
-            println!(
-                "{:?}",
-                args.get(0)
-                    .to_string(scope)
-                    .unwrap()
-                    .to_rust_string_lossy(scope)
-            );
-            let a: &String = KV.keys().next().unwrap();
-            rv.set(v8::String::new(scope, a).unwrap().into());
+            let key = args
+                .get(0)
+                .to_string(scope)
+                .unwrap()
+                .to_rust_string_lossy(scope);
+            let value = args
+                .get(1)
+                .to_string(scope)
+                .unwrap()
+                .to_rust_string_lossy(scope);
+            if let Some(prev) = KV.lock().unwrap().insert(key, value) {
+                rv.set(v8::String::new(scope, &prev).unwrap().into());
+            }
         },
     );
     let set_fn_name = v8::String::new(scope, "set").unwrap();
@@ -93,9 +90,8 @@ async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
     println!("javascript code: {}", code.to_rust_string_lossy(scope));
 
     let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script.run(scope).unwrap();
-    let result = result.to_string(scope).unwrap();
-    let result = result.to_rust_string_lossy(scope);
+    let output = script.run(scope).unwrap();
+    let result = output.to_string(scope).unwrap().to_rust_string_lossy(scope);
     println!("result: {}", result);
     Ok(result)
 }
