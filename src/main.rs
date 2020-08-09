f11673bb184801242704785369c32601aea6313c
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-08-08 18:33:22 -0700

    murder with extreme prejudice

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rusty_v8 as v8;
 
 use bytes::Bytes;
 use lazy_static::lazy_static;
-use log::trace;
+use log::{trace, warn};
 use std::collections::BTreeMap;
 use std::convert::Infallible;
 use std::sync::Mutex;
@@ -31,69 +31,81 @@ lazy_static! {
     static ref KV: Mutex<BTreeMap<String, String>> = Mutex::new(BTreeMap::new());
 }
 async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
-    let isolate = &mut v8::Isolate::new(Default::default());
+    Ok(tokio::task::spawn_blocking(move || {
+        let isolate = &mut v8::Isolate::new(Default::default());
+        let handle = isolate.thread_safe_handle();
 
-    let scope = &mut v8::HandleScope::new(isolate);
-    let context = v8::Context::new(scope);
-    let scope = &mut v8::ContextScope::new(scope, context);
+        let scope = &mut v8::HandleScope::new(isolate);
+        let context = v8::Context::new(scope);
+        let scope = &mut v8::ContextScope::new(scope, context);
 
-    let api_template = v8::ObjectTemplate::new(scope);
+        let api_template = v8::ObjectTemplate::new(scope);
 
-    let get_fn = v8::FunctionTemplate::new(
-        scope,
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            let key = args
-                .get(0)
-                .to_string(scope)
-                .unwrap()
-                .to_rust_string_lossy(scope);
-            if let Some(value) = KV.lock().unwrap().get(&key) {
-                rv.set(v8::String::new(scope, value).unwrap().into());
+        let get_fn = v8::FunctionTemplate::new(
+            scope,
+            |scope: &mut v8::HandleScope,
+             args: v8::FunctionCallbackArguments,
+             mut rv: v8::ReturnValue| {
+                let key = args
+                    .get(0)
+                    .to_string(scope)
+                    .unwrap()
+                    .to_rust_string_lossy(scope);
+                if let Some(value) = KV.lock().unwrap().get(&key) {
+                    rv.set(v8::String::new(scope, value).unwrap().into());
+                }
+            },
+        );
+        let get_fn_name = v8::String::new(scope, "get").unwrap();
+        api_template.set(get_fn_name.into(), get_fn.into());
+
+        let set_fn = v8::FunctionTemplate::new(
+            scope,
+            |scope: &mut v8::HandleScope,
+             args: v8::FunctionCallbackArguments,
+             mut rv: v8::ReturnValue| {
+                let key = args
+                    .get(0)
+                    .to_string(scope)
+                    .unwrap()
+                    .to_rust_string_lossy(scope);
+                let value = args
+                    .get(1)
+                    .to_string(scope)
+                    .unwrap()
+                    .to_rust_string_lossy(scope);
+                if let Some(prev) = KV.lock().unwrap().insert(key, value) {
+                    rv.set(v8::String::new(scope, &prev).unwrap().into());
+                }
+            },
+        );
+        let set_fn_name = v8::String::new(scope, "set").unwrap();
+        api_template.set(set_fn_name.into(), set_fn.into());
+
+        let global = context.global(scope);
+        let api_instance = api_template.new_instance(scope).unwrap();
+        let api_name = v8::String::new(scope, "api").unwrap();
+        global.set(scope, api_name.into(), api_instance.into());
+
+        let code = std::str::from_utf8(script.as_ref()).unwrap();
+        let code = v8::String::new(scope, &code).unwrap();
+        trace!("javascript code: {}", code.to_rust_string_lossy(scope));
+
+        tokio::spawn(async move {
+            trace!("i'm in another thread!");
+            tokio::time::delay_for(std::time::Duration::from_millis(50)).await;
+            if handle.terminate_execution() {
+                warn!("killing script after 50ms");
             }
-        },
-    );
-    let get_fn_name = v8::String::new(scope, "get").unwrap();
-    api_template.set(get_fn_name.into(), get_fn.into());
-
-    let set_fn = v8::FunctionTemplate::new(
-        scope,
-        |scope: &mut v8::HandleScope,
-         args: v8::FunctionCallbackArguments,
-         mut rv: v8::ReturnValue| {
-            let key = args
-                .get(0)
-                .to_string(scope)
-                .unwrap()
-                .to_rust_string_lossy(scope);
-            let value = args
-                .get(1)
-                .to_string(scope)
-                .unwrap()
-                .to_rust_string_lossy(scope);
-            if let Some(prev) = KV.lock().unwrap().insert(key, value) {
-                rv.set(v8::String::new(scope, &prev).unwrap().into());
-            }
-        },
-    );
-    let set_fn_name = v8::String::new(scope, "set").unwrap();
-    api_template.set(set_fn_name.into(), set_fn.into());
-
-    let global = context.global(scope);
-    let api_instance = api_template.new_instance(scope).unwrap();
-    let api_name = v8::String::new(scope, "api").unwrap();
-    global.set(scope, api_name.into(), api_instance.into());
-
-    let code = std::str::from_utf8(script.as_ref()).unwrap();
-    let code = v8::String::new(scope, &code).unwrap();
-    trace!("javascript code: {}", code.to_rust_string_lossy(scope));
-
-    let script = v8::Script::compile(scope, code, None).unwrap();
-    let output = script.run(scope).unwrap();
-    let result = output.to_string(scope).unwrap().to_rust_string_lossy(scope);
-    trace!("result: {}", result);
-    Ok(result)
+        });
+        let script = v8::Script::compile(scope, code, None).unwrap();
+        let output = script.run(scope).unwrap();
+        let result = output.to_string(scope).unwrap().to_rust_string_lossy(scope);
+        trace!("result: {}", result);
+        result
+    })
+    .await
+    .unwrap())
 }
 
 #[derive(StructOpt)]
