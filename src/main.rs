e69957c7c397aafa46d50f4810171ecfdfbdd92f
Ryan P. Brewster <RyanPBrewster@gmail.com>
2020-08-12 21:37:01 -0700

    add QPS tracking

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,22 @@ use rusty_v8 as v8;
 
 use bytes::Bytes;
 use lazy_static::lazy_static;
-use log::{trace, warn};
+use log::{info, trace, warn};
 use std::collections::BTreeMap;
 use std::convert::Infallible;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Duration;
 use std::{ops::Bound, sync::Mutex};
 use structopt::StructOpt;
 use warp::Filter;
 
+static QPS: AtomicUsize = AtomicUsize::new(0);
+
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    env_logger::Builder::from_default_env()
+        .format_timestamp_micros()
+        .init();
     let _opts = Opts::from_args();
 
     let platform = v8::new_default_platform().unwrap();
@@ -24,6 +30,14 @@ async fn main() {
         .and(warp::body::bytes())
         .and_then(exec_script);
     let routes = hello.or(exec);
+
+    tokio::spawn(async {
+        loop {
+            info!("qps: {}", QPS.swap(0, Ordering::SeqCst));
+            tokio::time::delay_for(Duration::from_millis(1_000)).await;
+        }
+    });
+
     warp::serve(routes).run(([127, 0, 0, 1], 9000)).await;
 }
 
@@ -31,7 +45,9 @@ lazy_static! {
     static ref KV: Mutex<BTreeMap<String, String>> = Mutex::new(BTreeMap::new());
 }
 async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
+    trace!("routed");
     Ok(tokio::task::spawn_blocking(move || {
+        trace!("spawned");
         let isolate = &mut v8::Isolate::new(Default::default());
         let handle = isolate.thread_safe_handle();
 
@@ -124,6 +140,7 @@ async fn exec_script(script: Bytes) -> Result<impl warp::Reply, Infallible> {
         let script = v8::Script::compile(scope, code, None).unwrap();
         let output = script.run(scope).unwrap();
         let result = output.to_string(scope).unwrap().to_rust_string_lossy(scope);
+        QPS.fetch_add(1, Ordering::SeqCst);
         trace!("result: {}", result);
         result
     })
